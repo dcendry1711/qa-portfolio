13882029999417b8fffc6b9dba483841c3988407
dcendry1711 <daniel.cendry@gmail.com>
2026-04-15 19:34:07 +0200

    update e2e.spec.ts

--- playwright-tests/tests/e2e.spec.ts
+++ playwright-tests/tests/e2e.spec.ts
@@ -7,7 +7,7 @@ import { CartPage } from "../pages/cart.page";
 import { checkoutPage } from "../pages/checkout.page";
 import { SummaryPage } from "../pages/summary.page";
 
-test("e2e test - complete order process", async ({ page }) => {
+test("E2E 01 - complete order process", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const inventoryPage = new InventoryPage(page);
   const cartPage = new CartPage(page);
