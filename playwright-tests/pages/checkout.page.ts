3609f4fa95f6b9dff9d73d40ad0f388b9f47170c
dcendry1711 <daniel.cendry@gmail.com>
2026-04-18 21:14:17 +0200

    update checkout.page.ts

--- playwright-tests/pages/checkout.page.ts
+++ playwright-tests/pages/checkout.page.ts
@@ -1,15 +1,20 @@
 import { Locator, Page } from "@playwright/test";
 
-export class checkoutPage {
+export class CheckoutPage {
+  checkoutHeaderTitle: Locator;
   checkoutContainer: Locator;
   firstName: Locator;
   lastName: Locator;
   postalCode: Locator;
-  continueBtn: Locator;
+  continueCheckoutBtn: Locator;
   subtotalPrice: Locator;
   taxPrice: Locator;
   totalPrice: Locator;
-  finishBtn: Locator;
+  finishCheckoutBtn: Locator;
+  tickIcon: Locator;
+  completeHeader: Locator;
+  completeParagraph: Locator;
+  backHomeBtn: Locator;
 
   async fillCheckoutForm(
     firstName: string,
@@ -22,22 +27,31 @@ export class checkoutPage {
   }
 
   async continueCheckout() {
-    await this.continueBtn.click();
+    await this.continueCheckoutBtn.click();
   }
 
   async finishCheckout() {
-    await this.finishBtn.click();
+    await this.finishCheckoutBtn.click();
+  }
+
+  async backToHome() {
+    await this.backHomeBtn.click();
   }
 
   constructor(private page: Page) {
+    this.checkoutHeaderTitle = page.locator(".title");
     this.checkoutContainer = page.locator(".checkout_info");
     this.firstName = page.locator('[data-test="firstName"]');
     this.lastName = page.locator('[data-test="lastName"]');
     this.postalCode = page.locator('[data-test="postalCode"]');
-    this.continueBtn = page.locator('[data-test="continue"]');
+    this.continueCheckoutBtn = page.locator('[data-test="continue"]');
     this.subtotalPrice = page.locator('[data-test="subtotal-label"]');
     this.taxPrice = page.locator('[data-test="tax-label"]');
     this.totalPrice = page.locator('[data-test="total-label"]');
-    this.finishBtn = page.locator('[data-test="finish"]');
+    this.finishCheckoutBtn = page.locator('[data-test="finish"]');
+    this.tickIcon = page.locator(".pony_express");
+    this.completeHeader = page.locator(".complete-header");
+    this.completeParagraph = page.locator(".complete-text");
+    this.backHomeBtn = page.locator('[data-test="back-to-products"]');
   }
 }
