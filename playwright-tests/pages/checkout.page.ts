239c502648cd16b2237e7020efff5d956e900049
dcendry1711 <daniel.cendry@gmail.com>
2026-04-19 17:21:06 +0200

    update on checkout.page.ts

--- playwright-tests/pages/checkout.page.ts
+++ playwright-tests/pages/checkout.page.ts
@@ -1,4 +1,5 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
+import { checkoutFormData } from "../data/checkoutForm.data";
 
 export class CheckoutPage {
   checkoutHeaderTitle: Locator;
@@ -6,7 +7,12 @@ export class CheckoutPage {
   firstName: Locator;
   lastName: Locator;
   postalCode: Locator;
+  errorMsg: Locator;
+  cancelCheckoutBtn: Locator;
   continueCheckoutBtn: Locator;
+  firstProduct: Locator;
+  secondProduct: Locator;
+  thirdProduct: Locator;
   subtotalPrice: Locator;
   taxPrice: Locator;
   totalPrice: Locator;
@@ -16,11 +22,34 @@ export class CheckoutPage {
   completeParagraph: Locator;
   backHomeBtn: Locator;
 
-  async fillCheckoutForm(
-    firstName: string,
-    lastName: string,
-    postalCode: string,
-  ) {
+  async checkoutPageStepOne() {
+    await expect(this.page).toHaveURL(/checkout-step-one.html/);
+    await expect(this.checkoutHeaderTitle).toHaveText("Checkout: Your Information");
+    await this.fillCheckoutForm(checkoutFormData.firstName,checkoutFormData.lastName,checkoutFormData.postalCode);
+    await this.continueCheckout();
+  }
+
+  async checkoutPageStepTwo() {
+    await expect(this.page).toHaveURL(/checkout-step-two.html/);
+    await expect(this.checkoutHeaderTitle).toHaveText("Checkout: Overview");
+    await expect(this.subtotalPrice).toHaveText("Item total: $55.97");
+    await expect(this.taxPrice).toHaveText("Tax: $4.48");
+    await expect(this.totalPrice).toHaveText("Total: $60.45");
+    await this.finishCheckout();
+  }
+
+  async finishCheckoutProcess() {
+    await expect(this.page).toHaveURL(/checkout-complete.html/);
+    await expect(this.checkoutHeaderTitle).toHaveText("Checkout: Complete!");
+    await expect(this.tickIcon).toBeVisible();
+    await expect(this.completeHeader).toHaveText("Thank you for your order!");
+    await expect(this.completeParagraph).toContainText("Your order has been dispatched");
+    await expect(this.backHomeBtn).toBeVisible();
+    await this.backToHome();
+    await expect(this.page).toHaveURL(/inventory.html/);
+  }
+
+  async fillCheckoutForm(firstName: string,lastName: string,postalCode: string) {
     await this.firstName.fill(firstName);
     await this.lastName.fill(lastName);
     await this.postalCode.fill(postalCode);
@@ -28,6 +57,18 @@ export class CheckoutPage {
 
   async continueCheckout() {
     await this.continueCheckoutBtn.click();
+  }
+
+  async validateProductsInOverviewPage() {
+    await expect(this.firstProduct).toContainText("Sauce Labs Backpack");
+    await expect(this.secondProduct).toContainText("Sauce Labs Bolt T-Shirt");
+    await expect(this.thirdProduct).toContainText("Sauce Labs Bike Light");
+  }
+
+  async validatePricesInOverviewPage() {
+    await expect(this.subtotalPrice).toHaveText("Item total: $55.97");
+    await expect(this.taxPrice).toHaveText("Tax: $4.48");
+    await expect(this.totalPrice).toHaveText("Total: $60.45");
   }
 
   async finishCheckout() {
@@ -44,7 +85,12 @@ export class CheckoutPage {
     this.firstName = page.locator('[data-test="firstName"]');
     this.lastName = page.locator('[data-test="lastName"]');
     this.postalCode = page.locator('[data-test="postalCode"]');
+    this.errorMsg = page.locator('[data-test="error"]');
+    this.cancelCheckoutBtn = page.locator('[data-test="cancel"]');
     this.continueCheckoutBtn = page.locator('[data-test="continue"]');
+    this.firstProduct = page.locator(".inventory_item_name").nth(0);
+    this.secondProduct = page.locator(".inventory_item_name").nth(1);
+    this.thirdProduct = page.locator(".inventory_item_name").nth(2);
     this.subtotalPrice = page.locator('[data-test="subtotal-label"]');
     this.taxPrice = page.locator('[data-test="tax-label"]');
     this.totalPrice = page.locator('[data-test="total-label"]');
