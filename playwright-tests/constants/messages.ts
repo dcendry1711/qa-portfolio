d3d01032a07bc77ad6261f82a8e50a79ff2e71fa
dcendry1711 <daniel.cendry@gmail.com>
2026-04-20 17:51:50 +0200

    create errorMessages.ts in constants dir

---
+++ playwright-tests/constants/messages.ts
@@ -0,0 +1,7 @@
+export const errorMessages = {
+  USER_REQUIRED: "Epic sadface: Username is required",
+  PASSWORD_REQUIRED: "Epic sadface: Password is required",
+  INVALID_CREDENTIALS:
+    "Epic sadface: Username and password do not match any user in this service",
+  LOCKED_USER: "Epic sadface: Sorry, this user has been locked out.",
+};
