cabfba11ad061d55cac8b6de5a30b6a13897db15
dcendry1711 <daniel.cendry@gmail.com>
2026-04-20 18:28:12 +0200

    create urls.ts

---
+++ playwright-tests/constants/urls.ts
@@ -0,0 +1,7 @@
+export const URLS = {
+  INVENTORY_URL: "/inventory.html",
+  CART_URL: "/cart.html",
+  CHECKOUT_STEP_ONE_URL: "/checkout-step-one.html",
+  CHECKOUT_STEP_TWO_URL: "/checkout-step-two.html",
+  CHECKOUT_COMPLETE_URL: "/checkout-complete.html",
+};
