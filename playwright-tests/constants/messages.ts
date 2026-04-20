19f31a46add12d89f427b514d9b07cc3705a60b8
dcendry1711 <daniel.cendry@gmail.com>
2026-04-20 18:28:03 +0200

    update messages.ts

--- playwright-tests/constants/messages.ts
+++ playwright-tests/constants/messages.ts
@@ -4,4 +4,7 @@ export const errorMessages = {
   INVALID_CREDENTIALS:
     "Epic sadface: Username and password do not match any user in this service",
   LOCKED_USER: "Epic sadface: Sorry, this user has been locked out.",
+  FIRST_NAME_REQUIRED: "Error: First Name is required",
+  LAST_NAME_REQUIRED: "Error: Last Name is required",
+  POSTAL_CODE_REQUIRED: "Error: Postal Code is required"
 };
