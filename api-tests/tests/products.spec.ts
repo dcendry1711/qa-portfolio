cf7024505b5035e23c1c3389b2b863cdb15ef1ea
dcendry1711 <daniel.cendry@gmail.com>
2026-04-25 21:24:38 +0200

    format document

--- api-tests/tests/products.spec.ts
+++ api-tests/tests/products.spec.ts
@@ -40,17 +40,17 @@ test.describe("Products API", () => {
     });
   });
 
-  test("TC04 - Delete product", async({fS}) => {
-    const deletedProductId = 1
-    const response = await fS.delete(`/products/${deletedProductId}`)
+  test("TC04 - Delete product", async ({ fS }) => {
+    const deletedProductId = 1;
+    const response = await fS.delete(`/products/${deletedProductId}`);
 
-    checkResponseStatus(response, 200)
-  })
+    checkResponseStatus(response, 200);
+  });
 
-  test("TC05 - Get non existing product", async({fS}) => {
+  test("TC05 - Get non existing product", async ({ fS }) => {
     const nonExistProductId = 9999;
-    const response = await fS.get(`/products/${nonExistProductId}`)
+    const response = await fS.get(`/products/${nonExistProductId}`);
 
-    checkResponseStatus(response,200) //In this test-case API should return status code 404 (not found)
-  })
+    checkResponseStatus(response, 200); //In this test-case API should return status code 404 (not found)
+  });
 });
