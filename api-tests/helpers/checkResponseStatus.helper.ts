84eff68fbf843c433e0ad9b46f91bf6b84d525e5
dcendry1711 <daniel.cendry@gmail.com>
2026-04-25 21:15:06 +0200

    chechResponseStatus helper created

---
+++ api-tests/helpers/checkResponseStatus.helper.ts
@@ -0,0 +1,10 @@
+import { APIResponse } from "@playwright/test";
+
+export function checkResponseStatus(response: APIResponse, expectedStatus: number): void {
+  const status = response.status();
+  if (status !== expectedStatus) {
+    throw new Error(`Expected status ${expectedStatus}, but got ${status}`);
+  } else {
+    console.log(`Status ${status} is as expected.`);
+  }
+}
