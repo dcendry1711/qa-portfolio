344e6bc11223f94c4104a65259cc707456800e58
dcendry1711 <daniel.cendry@gmail.com>
2026-04-29 18:49:55 +0200

    Update login.spec.ts

--- playwright-tests/tests/login.spec.ts
+++ playwright-tests/tests/login.spec.ts
@@ -7,74 +7,98 @@ const VALID_USERNAME = userLoginData.userName;
 const VALID_PASSWORD = userLoginData.password;
 
 test.describe("Login test cases - sauceDemo", () => {
-
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.navigateToLoginPage();
   });
-
-  test("LOGIN-TC01 - Successful login", async ({ loginPage, inventoryPage }) => {
+  //LOGIN-TC01 - Successful login
+  test("LOGIN-TC01 - Successful login", async ({
+    loginPage,
+    inventoryPage,
+  }) => {
     await loginPage.login(VALID_USERNAME, VALID_PASSWORD);
     await expect(inventoryPage.siteHeader).toContainText("Swag Labs");
   });
 
-  //LOGIN-TC02 - Invalid login
+  //LOGIN-TC02 - Invalid login with empty password
   test("LOGIN-TC02 - Login with wrong password", async ({ loginPage }) => {
     await loginPage.login(VALID_USERNAME, "");
-    await expect(loginPage.errorMsg).toHaveText(errorMessages.PASSWORD_REQUIRED);
+    await expect(loginPage.errorMsg).toHaveText(
+      errorMessages.PASSWORD_REQUIRED,
+    );
   });
-
+  //LOGIN-TC03 - Login without username
   test("LOGIN-TC03 - Login without username", async ({ loginPage }) => {
     await loginPage.login("", VALID_PASSWORD);
     await expect(loginPage.errorMsg).toHaveText(errorMessages.USER_REQUIRED);
   });
-
+  //LOGIN-TC04 - Login with invalid username
   test("LOGIN-TC04 - Login with invalid username", async ({ loginPage }) => {
     const invalidUser = "invalid_user";
 
     await loginPage.login(invalidUser, VALID_PASSWORD);
-    await expect(loginPage.errorMsg).toHaveText(errorMessages.INVALID_CREDENTIALS);
+    await expect(loginPage.errorMsg).toHaveText(
+      errorMessages.INVALID_CREDENTIALS,
+    );
   });
-
+  //LOGIN-TC05 - Login with invalid password
   test("LOGIN-TC05 - Login with invalid password", async ({ loginPage }) => {
     const invalidPassword = "wrong_pass";
 
     await loginPage.login(VALID_USERNAME, invalidPassword);
-    await expect(loginPage.errorMsg).toHaveText(errorMessages.INVALID_CREDENTIALS);
+    await expect(loginPage.errorMsg).toHaveText(
+      errorMessages.INVALID_CREDENTIALS,
+    );
   });
-
+  //LOGIN-TC06 - Login with empty fields
   test("LOGIN-TC06 - Login with empty fields", async ({ loginPage }) => {
     await loginPage.login("", "");
     await expect(loginPage.errorMsg).toHaveText(errorMessages.USER_REQUIRED);
   });
-
-  test("LOGIN-TC07 - Username with leading/trailing whitespace", async ({ loginPage }) => {
+  //LOGIN-TC07 - Login with username that has leading/trailing whitespace
+  test("LOGIN-TC07 - Username with leading/trailing whitespace", async ({
+    loginPage,
+  }) => {
     const userLogin = ` ${VALID_USERNAME} `; // Adding leading and trailing whitespace
 
     await loginPage.login(userLogin, VALID_PASSWORD);
-    await expect(loginPage.errorMsg).toHaveText(errorMessages.INVALID_CREDENTIALS);
+    await expect(loginPage.errorMsg).toHaveText(
+      errorMessages.INVALID_CREDENTIALS,
+    );
   });
-
-  test("LOGIN-TC08 - Password with leading/trailing whitespace", async ({ loginPage }) => {
+  //LOGIN-TC08 - Login with password that has leading/trailing whitespace
+  test("LOGIN-TC08 - Password with leading/trailing whitespace", async ({
+    loginPage,
+  }) => {
     const userPassword = ` ${VALID_PASSWORD} `;
 
     await loginPage.login(VALID_USERNAME, userPassword);
-    await expect(loginPage.errorMsg).toHaveText(errorMessages.INVALID_CREDENTIALS);
+    await expect(loginPage.errorMsg).toHaveText(
+      errorMessages.INVALID_CREDENTIALS,
+    );
   });
-
+  //LOGIN-TC09 - Login with locked user
   test("LOGIN-TC09 - Login with locked user", async ({ loginPage }) => {
     const lockedUser = "locked_out_user";
 
     await loginPage.login(lockedUser, VALID_PASSWORD);
     await expect(loginPage.errorMsg).toHaveText(errorMessages.LOCKED_USER);
   });
-
-  test("LOGIN-TC10 - Submit login with Enter key", async ({ page, loginPage, inventoryPage }) => {
+  //LOGIN-TC10 - Submit login with Enter key
+  test("LOGIN-TC10 - Submit login with Enter key", async ({
+    page,
+    loginPage,
+    inventoryPage,
+  }) => {
     await loginPage.loginWithEnterKey(VALID_USERNAME, VALID_PASSWORD);
     await expect(page).toHaveURL(URLS.INVENTORY_URL);
     await expect(inventoryPage.siteHeader).toBeVisible();
   });
-
-  test("LOGIN-TC11 - Refresh after successful login", async ({ page, loginPage, inventoryPage }) => {
+  //LOGIN-TC11 - Refresh after successful login
+  test("LOGIN-TC11 - Refresh after successful login", async ({
+    page,
+    loginPage,
+    inventoryPage,
+  }) => {
     await loginPage.login(VALID_USERNAME, VALID_PASSWORD);
     await page.reload();
     await expect(page).toHaveURL(URLS.INVENTORY_URL);
